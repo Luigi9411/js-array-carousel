f30e6c8613b8e833ce7ea3a4ed023b9199eadbbd
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-08 22:23:27 +0100

    add-bonus2

--- bonus2/js/script.js
+++ bonus2/js/script.js
@@ -6,9 +6,10 @@ const arrImages = [
 	'img/05.jpg',
 ];
 
-const eleSlider = document.querySelector('.slider');
+const eleSlider = document.querySelector('.slider-viewer');
 const eleButtonUp = document.querySelector('.button-up');
 const eleButtonDown = document.querySelector('.button-down');
+const eleSliderThumbs = document.querySelector('.thumbs');
 
 
 for (let i = 0; i < arrImages.length; i++) {
@@ -21,42 +22,52 @@ for (let i = 0; i < arrImages.length; i++) {
 	}
 
 	eleSlider.append(eleImg);
+
+	const eleThumb = document.createElement('img');
+	eleThumb.src = arrImages[i];
+	eleThumb.classList.add('thumb-img');
+	if (i === 0) {
+		eleThumb.classList.add('active');
+	}
+	eleSliderThumbs.append(eleThumb);
 }
 
 
 const listEleImg = document.querySelectorAll('.slider-img'); 
+const listThumbs = document.querySelectorAll('.thumb-img');
 
 let activeIndex = 0;
+
 
 
 eleButtonDown.addEventListener('click', function () {
 	
 	listEleImg[activeIndex].classList.remove('active');
-
+    listThumbs[activeIndex].classList.remove('active');
 	
 	activeIndex++;
-
+    if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
 	
 	listEleImg[activeIndex].classList.add('active');
-
-	eleButtonUp.classList.remove('hidden');
-	if (activeIndex === listEleImg.length - 1) {
-		eleButtonDown.classList.add('hidden');
-	}
+    listThumbs[activeIndex].classList.add('active');
+    ;
 });
 
 eleButtonUp.addEventListener('click', function () {
 	
 	listEleImg[activeIndex].classList.remove('active');
-
+    listThumbs[activeIndex].classList.remove('active');
 	
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
 	activeIndex--;
 
 	
 	listEleImg[activeIndex].classList.add('active');
-
-	eleButtonDown.classList.remove('hidden');
-	if (activeIndex === 0) {
-		eleButtonUp.classList.add('hidden');
-	}
+    listThumbs[activeIndex].classList.add('active');
+	
+	
 });
